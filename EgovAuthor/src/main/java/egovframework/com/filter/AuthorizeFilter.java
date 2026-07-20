c4e185f1f0c235a855eb8f6be3b9df978e87b0b9
hjlee1107 <pigmun2@naver.com>
2026-07-20 13:35:19 +0900

    NCSC 보안취약점 조치

--- EgovAuthor/src/main/java/egovframework/com/filter/AuthorizeFilter.java
+++ EgovAuthor/src/main/java/egovframework/com/filter/AuthorizeFilter.java
@@ -1,19 +1,23 @@
 package egovframework.com.filter;
 
+import egovframework.com.security.GatewayInternalAuthVerifier;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class AuthorizeFilter extends OncePerRequestFilter {
+
+    private final GatewayInternalAuthVerifier gatewayInternalAuthVerifier;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -30,5 +34,7 @@ public class AuthorizeFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI();
+        return path.matches(".*\\.(css|js|png|jpg|jpeg|gif|svg|woff|woff2|ttf|otf|eot|ico|html)$");
+    }
 
 }
